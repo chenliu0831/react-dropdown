9c8e6aa49f4426d63e693c232ed44b873a9da93b
Paul Melnikow <paul.melnikow@bodylabs.com>
2015-01-14 11:06:45 -0500

    Allow customizing class names and boilerplate text

--- index.js
+++ index.js
@@ -11,6 +11,44 @@ var Dropdown = React.createClass({
     value: React.PropTypes.object,
     options: React.PropTypes.array,
     onChange: React.PropTypes.func,
+
+    className: React.PropTypes.string,
+    isOpenClassName: React.PropTypes.string,
+    controlClassName: React.PropTypes.string,
+    arrowClassName: React.PropTypes.string,
+    placeholderClassName: React.PropTypes.string,
+
+    noSelectionMessage: React.PropTypes.string,
+
+    menuClassName: React.PropTypes.string,
+    optionClassName: React.PropTypes.string,
+    isSelectedClassName: React.PropTypes.string,
+    groupClassName: React.PropTypes.string,
+    groupTitleClassName: React.PropTypes.string,
+
+    noResultsMessage: React.PropTypes.string,
+    noResultsClassName: React.PropTypes.string,
+  },
+
+  getDefaultProps: function () {
+    return {
+      className: 'Dropdown',
+      isOpenClassName: 'is-open',
+      controlClassName: 'Dropdown-control',
+      arrowClassName: 'Dropdown-arrow',
+      placeholderClassName: 'placeholder',
+
+      noSelectionMessage: 'Select\u2026',
+
+      menuClassName: 'Dropdown-menu',
+      optionClassName: 'Dropdown-option',
+      isSelectedClassName: 'is-selected',
+      groupClassName: 'group',
+      groupTitleClassName: 'title',
+
+      noOptionsMessage: 'No options found',
+      noOptionsClassName: 'Dropdown-noresults',
+    };
   },
 
   getInitialState: function() {
@@ -22,7 +60,7 @@ var Dropdown = React.createClass({
 
   componentWillMount: function() {
     this.setState({
-      selected: this.props.value || { label: 'Select...', value: '' }
+      selected: this.props.value || { label: this.props.noSelectionMessage, value: '' }
     })
   },
 
@@ -59,24 +97,23 @@ var Dropdown = React.createClass({
   },
 
   renderOption: function (option) {
-    var optionClass = cx({
-      'Dropdown-option': true,
-      'is-selected': option == this.state.selected
-    })
+    var optionClasses = {};
+    optionClasses[this.props.optionClassName] = true;
+    optionClasses[this.props.isSelectedClassName] = option == this.state.selected;
 
-    return React.createElement("div", {key: option.value, className: optionClass, onMouseDown: this.setValue.bind(this, option), onClick: this.setValue.bind(this, option)}, option.label)
+    return React.createElement("div", {key: option.value, className: cx(optionClasses), onMouseDown: this.setValue.bind(this, option), onClick: this.setValue.bind(this, option)}, option.label)
   },
 
   buildMenu: function() {
     var ops = this.props.options.map(function(option) {
 
       if (option.type == 'group') {
-        var groupTitle = (React.createElement("div", {className: "title"}, option.name))
+        var groupTitle = (React.createElement("div", {className: this.props.groupTitleClassName}, option.name))
         var _options = option.items.map(function(item) {
           return this.renderOption(item)
         }.bind(this))
         return (
-          React.createElement("div", {className: "group", key: option.name}, 
+          React.createElement("div", {className: this.props.groupClassName, key: option.name}, 
             groupTitle, 
             _options
           )
@@ -87,25 +124,24 @@ var Dropdown = React.createClass({
 
     }.bind(this))
 
-    return ops.length ? ops : React.createElement("div", {className: "Dropdown-noresults"}, "No opitons found")
+    return ops.length ? ops : React.createElement("div", {className: this.props.noOptionsClassName}, this.props.noResultsMessage)
   },
 
   render: function() {
     var value = ''
 
-    value = (React.createElement("div", {className: "placeholder"}, this.state.selected.label))
-    var menu = this.state.isOpen ? React.createElement("div", {className: "Dropdown-menu"}, this.buildMenu()) : null
+    value = (React.createElement("div", {className: this.props.placeholderClassName}, this.state.selected.label))
+    var menu = this.state.isOpen ? React.createElement("div", {className: this.props.menuClassName}, this.buildMenu()) : null
 
-    var dropdownClass = cx({
-      'Dropdown': true,
-      'is-open': this.state.isOpen
-    })
+    var dropdownClasses = {};
+    dropdownClasses[this.props.className] = true;
+    dropdownClasses[this.props.isOpenClassName] = this.state.isOpen;
 
     return (
-      React.createElement("div", {className: dropdownClass}, 
-        React.createElement("button", {className: "Dropdown-control", onMouseDown: this.handleMouseDown, onTouchEnd: this.handleMouseDown}, 
+      React.createElement("div", {className: cx(dropdownClasses)}, 
+        React.createElement("button", {className: this.props.controlClassName, onMouseDown: this.handleMouseDown, onTouchEnd: this.handleMouseDown}, 
           value, 
-          React.createElement("span", {className: "Dropdown-arrow"})
+          React.createElement("span", {className: this.props.arrowClassName})
         ), 
         menu
       )
